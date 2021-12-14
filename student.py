3ff23df42ae64bdac869713394dd2d795c6e7d3e
coynek <91563299+coynek@users.noreply.github.com>
2021-12-14 19:25:23 +0000

    tehteyh

--- student.py
+++ student.py
@@ -48,7 +48,7 @@ class Piggy(PiggyParent):
                 "m": ("Katie's Robot moves Around Box", self.move_around_box),
                 "z": ("Katie's Slow Read", self.slow_read),
                 "l": ("Katie's L swerve test", self.swerve_left),
-                "t": ("Katie's R swerve test", self.swerve_right
+                "t": ("Katie's R swerve test", self.swerve_right)
                 }
         # loop and print the menu...
         for key in sorted(menu.keys()):
